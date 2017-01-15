41a7f77f7243210feefcb8ddad9df18b282bd0ba
Reggy <rgranovskiy@gmail.com>
2017-01-15 04:44:22 -0500

    Framework for processing and predictions based off of individual songs in game

---
+++ music.py
@@ -0,0 +1,12 @@
+import pyglet
+import songsprocess as sp
+from learn import clf
+import sys
+
+#select a song with a thing?, let's say file is in variable song?
+
+samples = sp.transform(sp.wav_to_amps(song))
+clusters = clf.predict(samples) #array of 0s 1s and 2s, a segment every 40ms
+#play song
+
+
