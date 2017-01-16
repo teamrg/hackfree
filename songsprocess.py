b1d15637effe93d1082bd9e268796163389afd69
Reggy <rgranovskiy@gmail.com>
2017-01-16 16:38:04 -0500

    Fixed syncing of song with level generation

--- songsprocess.py
+++ songsprocess.py
@@ -4,14 +4,16 @@ import scipy.io.wavfile as wf
 def wav_to_amps(filename):
     w = wave.open(filename, 'r') #open wave file as read only
     fr = w.getframerate() #need this for segmentation later
-    astr = w.readframes(w.getnframes()) #get frames
-    a = struct.unpack("%ih" % (w.getnframes()*w.getnchannels()), astr) #bit stuff
+    nframe = w.getnframes()
+    nchan = w.getnchannels()
+    astr = w.readframes(nframe) #get frames
+    a = struct.unpack("%ih" % (nframe*nchan), astr) #bit stuff
     a = [float(val) / pow(2,15) for val in a] #normalization
     w.close() #close
-    return a, fr; #return amplitudes array and the framerate
+    return a, fr*nchan; #return amplitudes array and the framerate
 
 def transform(proc, r):
-    l=r*.25 #segment length of .5s of frames
+    l=r*.25 #segment length of .25s of frames
     s=[] #contains segment
     samples = [] #all segments in song
     for frame in proc: 
